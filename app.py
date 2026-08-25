bbc96d1ac653e758c66144a556675fffdf51f549
javiercano731-lang <265157345+alvarojaviercano-ux@users.noreply.github.com>
2026-08-24 23:36:55 -0500

    Types of Machine Learning

--- app.py
+++ app.py
@@ -3,13 +3,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+
+
 @app.route('/')
-def home():
-    
-    return "HELLO WORLD"
-
-
-@app.route('/template')
 def pagina():
     return render_template('index.html')
 
