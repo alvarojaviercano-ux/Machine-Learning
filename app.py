783042ca000788dc1cb775f244468a80da3e3067
juliansantiagocanchontriana28-blip <juliansantiagocanchontriana28@gmail.com>
2026-08-25 02:06:05 -0500

    types of machine learning

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "hello world"
+
+@app.route("/template")
+def template():
+    return render_template("index.html")
