21be077673506ae54b82688bdc2bd1022e77f09a
Iris Lorena Penaloza <lorennapenaloza@gmail.com>
2026-09-04 16:10:05 -0500

    Set up Bootstrap navbar, base layout, Home page and route skeleton

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 
 
-
-
 @app.route('/')
-def pagina():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/concepts')
+def concepts():
+    return render_template('coming_soon.html', page_name="Concepts")
+
+
+@app.route('/types')
+def types():
+    return render_template('coming_soon.html', page_name="Types of Machine Learning")
+
+
+@app.route('/usecase/<int:n>')
+def usecase(n):
+    return render_template('coming_soon.html', page_name=f"Use Case {n}")
+
+
+@app.route('/regression/concepts')
+def regression_concepts():
+    return render_template('coming_soon.html', page_name="Linear Regression - Concepts")
+
+
+@app.route('/regression/application')
+def regression_application():
+    return render_template('coming_soon.html', page_name="Linear Regression - Application")
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    
+    app.run(debug=True)  
